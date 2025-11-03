cf478c458abdd43f2aba549892ef0377c3a75f9a
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2025-11-03 21:30:03 +0200

    fix: open contact links in new tab

--- src/components/common/contactsPopup.tsx
+++ src/components/common/contactsPopup.tsx
@@ -3,22 +3,25 @@ import { FiLinkedin, FiGithub, FiMail } from "react-icons/fi";
 
 const ContactsPopover = () => {
   return (
-    <div className="text-muted-foreground sticky top-55 hidden md:flex left-4 lg:left-10 mx-10 flex flex-col gap-4 absolute w-fit z-60">
+    <div className="text-muted-foreground absolute sticky top-55 left-4 z-60 mx-10 flex hidden w-fit flex-col gap-4 md:flex lg:left-10">
       <a
         href="https://www.linkedin.com/in/madalina-chirpicinic/"
-        className="w-fit rounded-full border-2 border-foreground/40 p-1 text-foreground/40 duration-400 hover:border-foreground hover:text-foreground"
+        target="_blank"
+        className="border-foreground/40 text-foreground/40 hover:border-foreground hover:text-foreground w-fit rounded-full border-2 p-1 duration-400"
       >
         <FiLinkedin className="size-6" />
       </a>
       <a
         href="https://github.com/MadalinaDev"
-        className="w-fit rounded-full border-2 border-foreground/40 p-1 text-foreground/40 duration-400 hover:border-foreground hover:text-foreground"
+        target="_blank"
+        className="border-foreground/40 text-foreground/40 hover:border-foreground hover:text-foreground w-fit rounded-full border-2 p-1 duration-400"
       >
         <FiGithub className="size-6" />
       </a>
       <a
         href="mailto:madalina.chirpicinic@gmail.com"
-        className="w-fit rounded-full border-2 border-foreground/40 p-1 text-foreground/40 duration-400 hover:border-foreground hover:text-foreground"
+        target="_blank"
+        className="border-foreground/40 text-foreground/40 hover:border-foreground hover:text-foreground w-fit rounded-full border-2 p-1 duration-400"
       >
         <FiMail className="size-6" />
       </a>
--- src/components/common/landingPage.tsx
+++ src/components/common/landingPage.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useRef } from "react";
 import Header from "../layout/header";
 import WhoAmI from "./whoAmI";
@@ -7,6 +8,8 @@ import Skills from "./skills";
 import Projects from "./projects";
 import Contact from "./contact";
 import ContactsPopover from "./contactsPopup";
+import { Pointer } from "../ui/pointer";
+import { useTheme } from "next-themes";
 
 const LandingPage = () => {
   const whoAmIRef = useRef<HTMLDivElement>(null);
@@ -48,9 +51,13 @@ const LandingPage = () => {
     return;
   };
 
+  const { theme } = useTheme();
+
+  console.log(theme);
+
   return (
-    <div className="w-full relative">
-      <Header handleClick={handleHeaderClick} /> 
+    <div className="relative w-full">
+      <Header handleClick={handleHeaderClick} />
       <ContactsPopover />
       <div className="mx-auto flex w-full max-w-[1580px] flex-col gap-y-2 px-5 md:px-32">
         <WhoAmI ref={whoAmIRef} />
@@ -59,6 +66,15 @@ const LandingPage = () => {
         <Skills ref={skillsRef} />
         <Contact ref={contactRef} />
       </div>
+      <Pointer key={theme}>
+        <div
+          className={`pointer-events-none absolute top-0 left-0 h-[180px] w-[180px] -translate-x-1/2 -translate-y-1/2 rounded-full blur-[24px] transition-all duration-300 ${
+            theme !== "dark"
+              ? "bg-[radial-gradient(circle_at_center,rgba(0,0,0,0.35)_0%,rgba(0,0,0,0.1)_20%,rgba(0,0,0,0)_60%)]"
+              : "bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.35)_0%,rgba(255,255,255,0.1)_20%,rgba(255,255,255,0)_60%)]"
+          }`}
+        />
+      </Pointer>
     </div>
   );
 };
