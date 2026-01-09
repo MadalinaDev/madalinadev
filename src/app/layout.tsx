b7313326f579a9ce5be233a7f9a1f526d5c22eb9
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2026-01-09 18:41:25 +0200

    updated links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import "@/styles/globals.css";
 
+import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
 import { ThemeProvider } from "next-themes";
+
+export const metadata: Metadata = {
+  title: "madalina.dev",
+  description:
+    "Personal blog and portfolio of Madalina Chirpicinic.",
+  icons: [{ rel: "icon", url: "/favicon.png" }],
+};
 
 const geist = Geist({
   subsets: ["latin"],
